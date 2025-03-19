f4da22986d030330eacab93ce0b020f335f3bbc4
MatveyRomodanovskiy <startpolus@gmail.com>
2025-03-19 19:06:27 +0300

    add logs

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersApi } from "../api/usersApi";
+import {loggerMiddleware} from "./loggerMiddleware";
 
 export const store = configureStore({
     reducer: {
         [usersApi.reducerPath]: usersApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(usersApi.middleware),
+        getDefaultMiddleware().concat(usersApi.middleware, loggerMiddleware),
 });
 
 // Типизация `RootState` и `AppDispatch`
